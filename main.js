8e9eca1a518c24f6cd0ef84d8dfd0ec7f32bac36
HARI <40994861+harymk@users.noreply.github.com>
2024-02-06 16:23:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,67 +1,33 @@
-const express = require('express');
-//import { MongoClient } from "mongodb";
-//require('dotenv').config()
-//console.log(process.env.MONGODB_CONNECTION_STRING) // remove this after you've confirmed it working
+import { WebSocketServer } from 'ws';
 
-const { MongoClient } = require("mongodb");
-MONGODB_CONNECTION_STRING='mongodb+srv://harymk:Urmine143@smarthome.2gnasl6.mongodb.net/?retryWrites=true&w=majority'
-
-const client = new MongoClient(MONGODB_CONNECTION_STRING);  // remove this after you've confirmed it working
+const wss = new WebSocketServer({ port: 8080 });
 
 
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
 
-
-app.get('/', (req, res) => {
-
-    res.sendFile(__dirname + '/index.html');
-   
-   });
-
-server.listen(3000, () => {
- console.log('listening on *:3000');
-});
-
-async function run() {
-
-    try {
+wss.on("connection", function (ws, req) {
+    console.log("Connection Opened");
+    console.log("Client size: ", wss.clients.size);
   
-      await client.connect();
-      const database = client.db('Device');
-      const messages = database.collection('Status');
-  
-      // Query for our test message:
-      //const query = { deviceid: "led" };
-      //const message = await messages.findOne(query);
-      //console.log(message);
-
-     // changeStream = messages.watch();
-      changeStream = messages.watch([], { fullDocument: 'updateLookup' });
-
-  // set up a listener when change events are emitted
-  changeStream.on("change", next => {
-      // process any change event
-      switch (next.operationType) {
-          case 'insert':
-              console.log(next.fullDocument.message);
-              break;
-          case 'update':
-              //console.log(next.updateDescription.updatedFields);
-              //console.log(next.documentKey._id);
-              console.log(next.fullDocument);
-      }
-  });
-  
-    } catch {
-  
-      // Ensures that the client will close when you error
-      await client.close();
+    if (wss.clients.size === 1) {
+      console.log("first connection. starting keepalive");
+      //keepServerAlive();
     }
-    
-
-  }
-
   
-  run().catch(console.dir);
+    ws.on("message", (data) => {
+      let stringifiedData = data.toString();
+      if (stringifiedData === 'pong') {
+        console.log('keepAlive');
+        return;
+      }
+      //broadcast(ws, stringifiedData, false);
+    });
+  
+    ws.on("close", (data) => {
+      console.log("closing connection");
+  
+      if (wss.clients.size === 0) {
+        console.log("last client disconnected, stopping keepAlive interval");
+       // clearInterval(keepAliveId);
+      }
+    });
+  });
